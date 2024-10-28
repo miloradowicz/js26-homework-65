7ed186b648bbd6a5cd66d755b95bd670e24d8f1f
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-28 06:10:15 +0600

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+import App from './App.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/' element={<App />} />)
+);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
