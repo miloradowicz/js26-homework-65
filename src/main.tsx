41ef36ca488cb8f6e3bdcf503bac7bd4ff90a626
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-30 14:25:17 +0600

    main: route structure fleshed out

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,33 @@ import {
 import '@fontsource/inter/cyrillic.css';
 
 import App from './App.tsx';
+import { CssBaseline, CssVarsProvider } from '@mui/joy';
+
+import Home from './containers/Home/Home.tsx';
+import Presenter from './containers/Presenter/Presenter.tsx';
+import Admin from './containers/Admin/Admin.tsx';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/' element={<App />} />)
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index element={<Home />} />
+      <Route path='pages'>
+        <Route path=':title' element={<Presenter />} />
+        <Route path='admin'>
+          <Route index element={<Admin />} />
+          <Route path=':title' element={<Admin />} />
+          <Route path='new' element={<Admin />} />
+        </Route>
+      </Route>
+    </Route>
+  )
 );
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CssVarsProvider>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </CssVarsProvider>
   </StrictMode>
 );
