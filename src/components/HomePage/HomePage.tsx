9c005ed5e36eb2cfa1bab31e7c29d53df7abf8d4
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-31 07:09:22 +0600

    Home component renamed and moved

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import ContentLayout from '../ContentLayout/ContentLayout';
+
+const HomePage = () => {
+  return (
+    <ContentLayout>
+      <h3>Welcome!</h3>
+      <p>Select a page in the navigation bar.</p>
+    </ContentLayout>
+  );
+};
+
+export default HomePage;
