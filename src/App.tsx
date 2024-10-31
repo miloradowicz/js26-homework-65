4b5198a8cf4e9632f6ba07814916eee9b6a04d09
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-31 07:11:25 +0600

    App finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,63 @@
-function App() {
-  return <></>;
+import { Box } from '@mui/joy';
+import { Outlet, useOutletContext } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import { PageWithUrl } from './types';
+import { useCallback, useEffect, useState } from 'react';
+import Api from './lib/api';
+
+interface Context {
+  reload: () => Promise<void>;
 }
+
+function App() {
+  const [pages, setPages] = useState<PageWithUrl[]>([]);
+
+  const loader = useCallback(async () => {
+    const data = await Api.getPages();
+
+    setPages(data ?? []);
+  }, []);
+
+  useEffect(() => {
+    loader();
+  }, [loader]);
+
+  const reload = useCallback(async () => {
+    await loader();
+  }, [loader]);
+
+  return (
+    <>
+      <Box
+        component='header'
+        sx={{
+          p: 2,
+          bgcolor: 'background.surface',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          borderBottom: '1px solid',
+          borderColor: 'divider',
+          position: 'sticky',
+          top: 0,
+          zIndex: 1100,
+        }}
+      >
+        <Navigation pages={pages} />
+      </Box>
+      <Box
+        component='main'
+        sx={{ p: 2, display: 'flex', justifyContent: 'center' }}
+      >
+        <Outlet context={{ reload } satisfies Context} />
+      </Box>
+    </>
+  );
+}
+
+export const useReload = () => {
+  return useOutletContext<Context>();
+};
 
 export default App;
