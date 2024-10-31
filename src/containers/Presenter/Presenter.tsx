0d985fa2305942917a1e518f0c60021da290e7da
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-31 07:10:25 +0600

    Presenter component added

--- src/containers/Presenter/Presenter.tsx
+++ src/containers/Presenter/Presenter.tsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Page } from '../../types';
+import Api from '../../lib/api';
+import { Box } from '@mui/joy';
+import ContentLayout from '../../components/ContentLayout/ContentLayout';
+
 const Presenter = () => {
-  return <></>;
+  const { url } = useParams();
+  const [page, setPage] = useState<Page>();
+  const [error, setError] = useState<unknown>();
+
+  useEffect(() => {
+    const loader = async () => {
+      try {
+        if (!url) {
+          throw new Error('Path not found.');
+        }
+
+        const data = await Api.getPage(url);
+
+        if (!data) {
+          throw new Error('Page not found.');
+        }
+
+        setPage(data);
+      } catch (err) {
+        setError(err);
+      }
+    };
+
+    loader();
+  }, [url]);
+
+  useEffect(() => {
+    if (error) {
+      throw error;
+    }
+  }, [error]);
+
+  return (
+    <ContentLayout>
+      <h3>{page?.title}</h3>
+      <Box>{page?.content}</Box>
+    </ContentLayout>
+  );
 };
 
 export default Presenter;
