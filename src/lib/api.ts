7f6bb01235a940e3e39c148a5998071be7d794ec
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-31 07:09:01 +0600

    types finished, api livrary finished

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,16 +1,22 @@
 import axios from 'axios';
-import { Page } from '../types';
+import dashify from 'dashify';
+
+import { Id, Page, PageWithUrl } from '../types';
 
 const baseUrl =
   'https://js26-na-default-rtdb.europe-west1.firebasedatabase.app/hw-65/';
 
-type ApiPage = Omit<Page, 'id'>;
+type ApiPage = PageWithUrl;
+
+type ApiId = {
+  name: string;
+};
 
 type ApiPageContainer = {
   [name: string]: ApiPage;
 };
 
-export const getPages = async (): Promise<Page[]> => {
+const getPages = async (): Promise<PageWithUrl[]> => {
   const endpoint = 'pages.json';
 
   const { data, status } = await axios.get<ApiPageContainer | null>(
@@ -18,29 +24,105 @@ export const getPages = async (): Promise<Page[]> => {
   );
 
   if (status !== 200) {
-    throw new Error();
+    throw new Error(`Server response: ${status}`);
   }
 
-  return (
-    (data && Object.entries(data).map(([id, page]) => ({ id, ...page }))) || []
-  );
+  return (data && Object.entries(data).map(([, page]) => ({ ...page }))) || [];
 };
 
-export const getPage = async (id: string): Promise<Page | null> => {
-  const endpoint = `pages/${id}.json`;
+const createPage = async (page: Page): Promise<Id> => {
+  const endpoint = `pages.json`;
 
-  const { data, status } = await axios.get<ApiPage | null>(
+  const { data, status } = await axios.post<ApiId | null>(
+    new URL(endpoint, baseUrl).href,
+    { ...page, url: dashify(page.title) }
+  );
+
+  if (status !== 200) {
+    throw new Error(`Server response: ${status}`);
+  }
+
+  if (!data) {
+    throw new Error('Could not create page.');
+  }
+
+  return { id: data.name };
+};
+
+const getPage = async (url: string): Promise<PageWithUrl> => {
+  const endpoint = `pages.json?orderBy="url"&equalTo="${url}"`;
+
+  const { data, status } = await axios.get<ApiPageContainer | null>(
     new URL(endpoint, baseUrl).href
   );
 
   if (status !== 200) {
-    throw new Error();
+    throw new Error(`Server response: ${status}`);
   }
 
-  return (data && { id, ...data }) || null;
+  if (!data) {
+    throw new Error('Page not found.');
+  }
+
+  const _data = Object.entries(data).map(([, page]) => ({ ...page }));
+
+  if (_data.length === 0) {
+    throw new Error('Page not found.');
+  }
+
+  if (_data.length !== 1) {
+    throw new Error('Path is not unique.');
+  }
+
+  return _data[0];
 };
 
-export const deletePage = async (id: string): Promise<null> => {
+const getPageId = async (url: string): Promise<ApiId> => {
+  const endpoint = `pages.json?orderBy="url"&equalTo="${url}"`;
+
+  const { data, status } = await axios.get<ApiPageContainer | null>(
+    new URL(endpoint, baseUrl).href
+  );
+
+  if (status !== 200) {
+    throw new Error(`Server response: ${status}`);
+  }
+
+  if (!data) {
+    throw new Error('Page not found.');
+  }
+
+  const _data = Object.entries(data).map(([name]) => ({ name }));
+
+  if (_data.length === 0) {
+    throw new Error('Page not found.');
+  }
+
+  if (_data.length !== 1) {
+    throw new Error('Path is not unique.');
+  }
+
+  return _data[0];
+};
+
+const updatePage = async (url: string, page: Page): Promise<Page | null> => {
+  const _page = await getPageId(url);
+
+  const endpoint = `pages/${_page.name}.json`;
+
+  const { data, status } = await axios.put<Page | null>(
+    new URL(endpoint, baseUrl).href,
+    { ...page, url: dashify(page.title) }
+  );
+
+  if (status !== 200) {
+    throw new Error(`Server response: ${status}`);
+  }
+
+  return data && { ...data };
+};
+
+const deletePage = async (id: string): Promise<null> => {
   const endpoint = `pages/${id}.json`;
 
   const { data, status } = await axios.delete<null>(
@@ -48,8 +130,16 @@ export const deletePage = async (id: string): Promise<null> => {
   );
 
   if (status !== 200) {
-    throw new Error();
+    throw new Error(`Server response: ${status}`);
   }
 
   return data;
+};
+
+export default {
+  getPages,
+  createPage,
+  getPage,
+  updatePage,
+  deletePage,
 };
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,18 @@
 export interface Page {
   get title(): string;
-  get id(): string;
   get content(): string;
+}
+
+export interface Id {
+  get id(): string;
+}
+
+export interface Url {
+  get url(): string;
+}
+
+export interface PageWithUrl extends Page, Url {}
+
+export interface InvalidationObject {
+  c: null;
 }
